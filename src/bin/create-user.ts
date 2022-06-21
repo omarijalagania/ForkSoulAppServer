f9965d5a0db370832c73a9ec851166b27853b1fa
omari <omari.jalagania@gmail.com>
2022-06-21 21:52:10 +0400

    fix: modify user register

--- src/bin/create-user.ts
+++ src/bin/create-user.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs'
 import { User } from '../models'
 import { validateRegister } from '../schema'
 import { connectDB } from '../config'
+import { UserSchemaProps } from '../types'
 import prompt from 'prompt'
 
 prompt.start()
@@ -15,46 +16,42 @@ prompt.get(
     //@ts-ignore
     { name: 'confirmPassword', hidden: true, replace: '*' },
   ],
-  async function (_, result) {
+  async function (_, result: UserSchemaProps) {
     const results = {
       user_name: result.user_name,
       password: result.password,
       confirmPassword: result.confirmPassword,
     }
-    //@ts-ignore
+
     const { error } = validateRegister(results)
 
     if (error) {
       return console.log(error.details[0].message)
     }
 
-    let connection = connectDB(false)
-    //@ts-ignore
-    if (connection) {
-      try {
-        const user = await User.findOne({ user_name: result.user_name })
+    connectDB(false)
 
-        if (user) {
-          console.log('User already exists')
+    try {
+      const user = await User.findOne({ user_name: result.user_name })
 
-          return connectDB(true)
-        }
-        const salt = bcrypt.genSaltSync(10)
-        //@ts-ignore
-        const hashedPassword = bcrypt.hashSync(result.password, salt)
-
-        const newUser = await User.create({
-          user_name: result.user_name,
-          password: hashedPassword,
-        })
-
-        if (newUser) {
-          console.log('User created')
-          connectDB(true)
-        }
-      } catch (error) {
-        console.log(error)
+      if (user) {
+        console.log('User already exists')
+        return connectDB(true)
       }
+      const salt = bcrypt.genSaltSync(10)
+      const hashedPassword = bcrypt.hashSync(result.password, salt)
+
+      const newUser = await User.create({
+        user_name: result.user_name,
+        password: hashedPassword,
+      })
+
+      if (newUser) {
+        console.log('User created')
+        connectDB(true)
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 )
