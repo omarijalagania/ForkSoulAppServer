30c4666652ef02288c1f107ceaf76a4da403353b
omari <omari.jalagania@gmail.com>
2022-06-21 11:27:10 +0400

    chore: initial commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("hello")
+})
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
