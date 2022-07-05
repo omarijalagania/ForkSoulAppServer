17460a7b7b054233d9f3692c895d1b3cd07df00c
omari <omari.jalagania@gmail.com>
2022-07-05 23:28:14 +0400

    chore: change swagger.yaml server name

--- src/middlewares/swagger-middleware.ts
+++ src/middlewares/swagger-middleware.ts
@@ -2,7 +2,7 @@ import SwaggerUI from 'swagger-ui-express'
 import YAML from 'yamljs'
 
 const swaggerMiddleware = () => {
-  const swaggerDocument = YAML.load('src/config/swagger.yaml')
+  const swaggerDocument = YAML.load('./src/config/swagger.yaml')
   return [
     SwaggerUI.serve,
     SwaggerUI.setup(swaggerDocument, {
