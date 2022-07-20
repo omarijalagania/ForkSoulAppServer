5c9558f20a07add495829ee9e0c4e3315d4255c2
omari <omari.jalagania@gmail.com>
2022-07-20 10:59:52 +0400

    fix: add trycatch to auth

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -7,24 +7,28 @@ import { User } from '../models'
 export const userAuth = async (req: Request, res: Response) => {
   const { error } = validateLogin(req.body)
 
-  if (error) {
-    return res.status(422).send(error.details[0].message)
+  try {
+    if (error) {
+      return res.status(422).send(error.details[0].message)
+    }
+
+    const user = await User.findOne({ user_name: req.body.user_name })
+
+    if (!user) {
+      return res.status(422).send('Please provide valid credentials')
+    }
+
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if (!validPass) {
+      return res.status(422).send('Please provide valid credentials')
+    }
+
+    const token = jwt.sign(
+      { _id: user._id, name: user.user_name },
+      process.env.TOKEN_SECRET
+    )
+    res.header('auth-token', token).send({ token: token })
+  } catch (error) {
+    res.status(500).send({ error: 'something went wrong...' })
   }
-
-  const user = await User.findOne({ user_name: req.body.user_name })
-
-  if (!user) {
-    return res.status(422).send('Please provide valid credentials')
-  }
-
-  const validPass = await bcrypt.compare(req.body.password, user.password)
-  if (!validPass) {
-    return res.status(422).send('Please provide valid credentials')
-  }
-
-  const token = jwt.sign(
-    { _id: user._id, name: user.user_name },
-    process.env.TOKEN_SECRET
-  )
-  res.header('auth-token', token).send({ token: token })
 }
